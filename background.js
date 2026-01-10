80dfc2f53f75b547e43317f8497c236a6734e387
Barkai Lazimy <barkailazimy@gmail.com>
2026-01-09 23:40:41 -0600

    bundling everything in background

--- background.js
+++ background.js
@@ -1,4 +1,154 @@
 
+const computeShaderCode = `struct Neuron{
+    state: f32,
+    fired: f32,
+    connections: array<f32, 8>,
+}
+
+@group(0) @binding(0) var<uniform> grid: vec2f;
+
+@group(0) @binding(1) var<storage> networkIn: array<Neuron>;
+@group(0) @binding(2) var<storage, read_write> networkOut: array<Neuron>;
+
+fn cellIndex(cell: vec2u) -> u32 {
+    return (cell.y % (u32(grid.y)) * u32(grid.x)) + (cell.x % u32(grid.x));
+}
+
+fn cellActive(x: u32, y: u32) -> Neuron {
+    return networkIn[cellIndex(vec2(x, y))];
+}
+
+// fn getConnection(cell: vec2u, n: i32) -> u32 {
+//     switch (n){
+//         case 0: {
+//             if(cell.x-1 >= 0 && cell.y-1 >= 0 && cell.x-1 < u32(grid.x) && cell.y-1 < u32(grid.y)){
+//                 return cellIndex(vec2u(cell.x-1, cell.y-1));
+//             }
+//         }
+//         default: {
+//             return 4294967295;
+//         }
+//     }
+//     return 4294967295;
+// }
+
+fn getConnection(n: i32) -> vec2f {
+    switch (n){
+        case 0: {return vec2f(-1,-1);}
+        case 1: {return vec2f(0,-1);}
+        case 2: {return vec2f(1,-1);}
+        case 3: {return vec2f(1,0);}
+        case 4: {return vec2f(1,1);}
+        case 5: {return vec2f(0,1);}
+        case 6: {return vec2f(-1,1);}
+        case 7: {return vec2f(-1,0);}
+        default: {return vec2f(0,0);}
+    }
+}
+
+fn updateNeuron(index: u32){
+    const sDecay = f32(0.976);
+    const fDecay = f32(0.9);
+    const activation = f32(0.2);
+
+    networkOut[index].fired = networkIn[index].fired * fDecay;
+
+    if(networkIn[index].state > activation){
+        networkOut[index].state = 1;
+        networkOut[index].fired = 1;
+    }else{
+        networkOut[index].state = networkIn[index].state * sDecay;
+    }
+
+    if(networkIn[index].state >= f32(1)){
+        networkOut[index].state = -1;
+    }
+}
+
+@compute @workgroup_size(8, 8)
+fn computeMain(@builtin(global_invocation_id) cell: vec3u) {
+    let i = cellIndex(cell.xy);
+    // update node
+    updateNeuron(i);
+
+    networkOut[i].connections = networkIn[i].connections;
+
+
+    for(var j = 0; j < 8; j++){
+        let con = getConnection(j);
+        let cord = vec2f(cell.xy)+con;
+        if(cord.x >= 0 && cord.y >= 0 && cord.x < grid.x && cord.y < grid.y){
+            if(networkIn[i].state == 1){
+                let connection = cellIndex(vec2u(cord));
+                let w = ((j+3)%8); // weight of other node
+                networkOut[connection].state += networkIn[i].connections[j];
+
+                // train
+                const MAXWEIGHT = 0.1;
+                const tmax = 1; // max training function value
+                const tmin = -0.2; // min training function value
+                const LR = 0.6;
+                // const rThreshold = 10.5; // rienforcement threshold
+                // const fDecay = Math.pow(-tmin/(tmax-tmin), 1/rThreshold);
+                networkOut[connection].connections[w] += ((networkIn[i].fired*(tmax-tmin)+tmin)) * (MAXWEIGHT-(networkIn[connection].connections[w])) * LR;
+            }
+        }
+
+    }
+
+    // let con = vec2f(1,-1);
+    // let cord = vec2f(cell.xy);
+    // networkOut[i].connections[0] = f32((cord.x+1)/grid.x);
+    // networkOut[i].connections[1] = f32((cord.y+1)/grid.y);
+
+}`
+
+const renderShaderCode = `
+struct Neuron{
+    state: f32,
+    fired: f32,
+    connections: array<f32, 8>,
+}
+
+struct VertexOutput {
+    @builtin(position) position: vec4f,
+    @location(0) cell: vec2f,
+    @location(1) color: vec4f,
+};
+
+@group(0) @binding(0) var<uniform> grid: vec2f;
+@group(0) @binding(1) var<storage> network: array<Neuron>;
+
+@vertex
+fn vertexMain(@location(0) position: vec2f, @builtin(instance_index) instance: u32) -> VertexOutput {
+    var output: VertexOutput;
+
+    let node = network[instance];
+
+    let i = f32(instance);
+    let cell = vec2f(i % grid.x, floor(i / grid.x));
+
+    let scale = f32(1);//f32(cellState[instance]);
+    let cellOffset = cell / grid * 2;
+    let gridPos = (position*scale+1) / grid - 1 + cellOffset;
+
+    output.position = vec4f(gridPos, 0, 1);
+    output.cell = cell / grid;
+
+    var connectivity = 0f;
+    for(var j = 0; j < 8; j++){
+        connectivity+= node.connections[j];
+    }
+    connectivity/=8;
+    output.color = vec4f(0, node.fired, -min(node.state, 0), 1);
+    return output;
+}
+
+@fragment
+fn fragmentMain(input: VertexOutput) -> @location(0) vec4f {
+    return input.color;
+}`
+
 export default async function createBackground(canvas, width){
     if (!navigator.gpu) throw Error("WebGPU not supported.");
 
@@ -7,9 +157,6 @@ export default async function createBackground(canvas, width){
 
     const device = await adapter.requestDevice();
     if (!device) throw Error("Couldn’t request WebGPU logical device.");
-
-    const renderShaderCode = await loadFile('./renderBackground.wgsl');
-    const computeShaderCode = await loadFile('./compute.wgsl');
 
     const context = canvas.getContext("webgpu");
     const canvasFormat = navigator.gpu.getPreferredCanvasFormat();
@@ -210,29 +357,6 @@ export default async function createBackground(canvas, width){
 
         device.queue.writeBuffer(networkBufferStorage[step % 2], 4*cell, data);
 
-        //activate
-        // data[cell] = value;
-
-        // write
-        // device.queue.writeBuffer(networkBufferStorage[step % 2], 0, data);
-
-        // read
-        // await resultsBuffer.mapAsync(
-        //     GPUMapMode.READ,
-        //     0, // Offset
-        //     BUFFER_SIZE*4, // Length
-        // );
-        // const copyArrayBuffer = resultsBuffer.getMappedRange(0, BUFFER_SIZE*4);
-        // const data = new Float32Array(copyArrayBuffer.slice(0));
-        // resultsBuffer.unmap();
-
-        // //activate
-        // data[cell] = value;
-
-        // // write
-        // device.queue.writeBuffer(networkBufferStorage[step % 2], 0, data);
-
-
     }
 
     async function readNetwork(){
@@ -251,33 +375,6 @@ export default async function createBackground(canvas, width){
     function positionToIndex(x, y){
         return (x + y * GRID_WIDTH) * 10;
     }
-
-    //event stuff
-    const inputKeys = ' abcdefghijklmnopqrstuvwxyz1234567890'
-    var input = {}
-    // generate input
-    for(var i = 0; i < inputKeys.length; i++){
-        const key = inputKeys[i];
-        input[key] = {
-            x: Math.floor((i / inputKeys.length) * GRID_WIDTH),
-            y: 10,
-            value: 0
-        }
-    }
-
-    document.addEventListener('keydown', async (event)=>{
-        input[event.key].value = 1;
-        // update inputs
-        // await updateInput()
-        await activate(5,5,1)
-        await activate(5,10,1)
-        await activate(10,7, -1)
-    })
-    document.addEventListener('keyup', async (event)=>{
-        input[event.key].value = 0;
-        // update inputs
-        // await updateInput()
-    })
 
     canvas.addEventListener('mousedown', async (event)=>{
         var rect = event.target.getBoundingClientRect();
@@ -329,10 +426,5 @@ export default async function createBackground(canvas, width){
         device.queue.submit([encoder.finish()]);
     }
 
-    async function loadFile(src){
-        const response = await fetch(src);
-        const data = await response.text();
-        return data;
-    }
     return {update, readNetwork, positionToIndex, activate};
 }
